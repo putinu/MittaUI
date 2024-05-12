60f1ea95a383eac7dd0b761dda53bb10699c91f3
putinu <putinu2521@gmail.com>
2024-05-13 00:37:45 +0900

    [add] ColroToggleButtonViewの作成

---
+++ Assets/Plugins/MittaUI/MittaUI.Runtime/View/ColorToggleButtonView.cs
@@ -0,0 +1,32 @@
+﻿using MittaUI.Runtime.Constant;
+using MittaUI.Runtime.Parts;
+using UnityEngine;
+using uPalette.Runtime.Core;
+
+namespace MittaUI.Runtime
+{
+    /// <summary>
+    /// 色変化式トグルボタン
+    /// </summary>
+    [AddComponentMenu(AddComponentMenuConst.ButtonBath + nameof(ColorToggleButtonView))]
+    public class ColorToggleButtonView : BaseToggleButton
+    {
+#if MITTAUI_USE_UPALETTE
+        [SerializeField] private ColorEntryId _activeColorEntryId;
+        [SerializeField] private ColorEntryId _inactiveColorEntryId;
+        // TODO: 色を変化させる対象のGraphicを指定する
+#endif
+        
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            OnToggleChangedCallback = null;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+        }
+    }
+}
